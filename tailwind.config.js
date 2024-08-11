534735ae6b886005b03998c58c793da6f6368a7b
Tom <JosephDotson@belkonar.com>
2024-08-11 19:24:31 -0400

    updated styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
         white: '#f2f4f8',
         on: '#f16da6',
         code: '#c8a5ff',
-        muted: '#8cb6ff'
+        headers: '#8cb6ff',
+        muted: '#52bdff'
       }
     },
   },
